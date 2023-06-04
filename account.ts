8d09e7663f51699ec8ac50d4f312e44dc493143b
Luis Fernando <guffenix@gmail.com>
2023-06-04 16:35:17 -0500

    feat: create account

---
+++ account.ts
@@ -0,0 +1,13 @@
+import algosdk from "algosdk";
+
+export const createAccount = async() =>{
+    const account = algosdk.generateAccount();
+    const mnemonic = algosdk.secretKeyToMnemonic(account.sk);
+
+    console.log("Cuenta nueva creada");
+    console.log(`==> Address: ${account.addr}`);
+    console.log(`==> Secret key: ${account.sk}`);
+    console.log(`==> Frase semilla: ${mnemonic}`);
+    return account;
+
+}
