e6be1a44aadeaa042932777d89ad74ccc464b319
Luis Fernando <guffenix@gmail.com>
2023-06-05 23:14:24 -0500

    feat: scan inputs

---
+++ src/utils.ts
@@ -0,0 +1,18 @@
+import readline from 'readline'
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+const skipPrompts = process.argv.includes('--skip-prompts')
+
+export const waitForInputs = async () => {
+  if (skipPrompts === true)
+    return new Promise((resolve) => {
+      resolve(true)
+    })
+  return new Promise((resolve) => {
+    rl.question('Press enter to continue...', resolve)
+  })
+}
